a14135dae3db16e3b5794f6a5a36e60176172fa5
Dievano Dantas <dievano_dantas@hotmail.com>
2022-09-28 22:46:34 -0300

    melhora na estruturação dos dados + adição de novas informações sobre o clima

--- src/controllers/weatherContoller.js
+++ src/controllers/weatherContoller.js
@@ -1,48 +1,95 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const dotenv = require('dotenv');
+const { removeAllLetters } = require('../utils/tools');
 
 dotenv.config({ path: './src/config/.env' });
 
 const getWeather = async (req, res) => {
-    // const TEMPERATURE_AMOUNT_TODAY = 4;
-    // const TEMPERATURE_AMOUNT_HOUR_DAYS = 5;
 
     let response = await axios.get(process.env.URL_BASE);
 
     const $ = cheerio.load(response.data);
 
-    let sun = $('.SunriseSunset--dateValue--N2p5B').text();
-    let sunrise = sun.substring(0, sun.length - 5);
-    let sunset = sun.slice(-5);
-
-    let score = $('[data-testid="DonutChartValue"]').text();
-    let quality = $('[data-testid="AirQualityCategory"]').text();
-    let description = $('[data-testid="AirQualitySeverity"]').text();
-
     const weather = {
         location: $('h1.CurrentConditions--location--kyTeL').text(),
         temperature: $('span.CurrentConditions--tempValue--3a50n').text(),
         condition: $('.CurrentConditions--phraseValue--2Z18W').text(),
+        thermalSensation: $('[data-testid="FeelsLikeSection"] > [data-testid="TemperatureValue"]').text(),
         wind: $('span.Wind--windWrapper--3aqXJ > :last-child')[0].next.data,
         humidity: $('span[data-testid="PercentageValue"]').text(),
+        dewPoint: $('[data-testid="WeatherDetailsListItem"]:nth-child(4) > .WeatherDetailsListItem--wxData--2s6HT > span').text(),
         visibility: $('[data-testid="VisibilityValue"]').text(),
         moon: $('.TodayDetailsCard--detailsContainer--16Hg0 > :last-child > :last-child')[0].lastChild.data,
-        airQuality: { score, quality, description },
-        sun: { sunrise, sunset },
+        climateVariation: getMaxMinTemperatureData($),
+        airQuality: getAirQualityData($),
+        sun: getSunData($),
+        forecastNextDays: getWeatherNextFiveDays($)
     };
 
-    let arrTemps = [];
-
-    $('[data-testid="SegmentHighTemp"] > span').map((index, element) => {
-        arrTemps.push(element.firstChild.data);
-    });
-
-    console.log("Hoje: " + arrTemps.slice(0, 4));
-    console.log("Hora: " + arrTemps.slice(4, 9));
-    console.log("Dias: " + arrTemps.slice(9, 14));
-
     return res.send(weather);
+}
+
+const getMaxMinTemperatureData = ($) => {
+    let temps = [];
+    $('.TodayDetailsCard--detailsContainer--16Hg0 > :first-child .WeatherDetailsListItem--wxData--2s6HT > span[data-testid="TemperatureValue"]').map((index, element) => {
+        temps.push(element.firstChild.data)
+    })
+
+    return { max: temps[0], min: temps[1] };
+}
+
+const getAirQualityData = ($) => {
+    let score = $('[data-testid="DonutChartValue"]').text();
+    let quality = $('[data-testid="AirQualityCategory"]').text();
+    let description = $('[data-testid="AirQualitySeverity"]').text();
+
+    return { score, quality, description };
+}
+
+const getSunData = ($) => {
+    let sun = $('.SunriseSunset--dateValue--N2p5B').text();
+    let sunrise = sun.substring(0, sun.length - 5);
+    let sunset = sun.slice(-5);
+
+    return { sunrise, sunset };
+}
+
+const getWeatherNextFiveDays = ($) => {
+    let parentElementArray = [], childElementsArray = [];
+
+    $('.DailyWeatherCard--TableWrapper--3mjsg').find('ul > li > a').each((index, element) => {
+        parentElementArray.push($(element));
+
+        $(parentElementArray[index]).find('span:not(.Accessibility--visuallyHidden--2uGW3)').each((index, element) => {
+            childElementsArray.push($(element).text());
+        });
+    });
+    return organizeDailyDataArray(childElementsArray);
+}
+
+const organizeDailyDataArray = (arr) => {
+    const NUMBER_OBJECT_FIELDS = 4;
+    let arrTemp = [];
+    let arrDailyData = [];
+    let count = 0;
+
+    for (let index = 0; index <= arr.length; index++) {
+        if (count === NUMBER_OBJECT_FIELDS) {
+            arrDailyData.push(arrTemp);
+            arrTemp = [];
+            count = 0;
+        }
+        arrTemp.push(arr[index]);
+        count++;
+    }
+
+    return convertArrayToForecastObject(arrDailyData);
+}
+
+const convertArrayToForecastObject = (arr) => {
+    const obj = arr.map(([day, max, min, rain]) => ({ day, max, min, probabilityRain: removeAllLetters(rain) }));
+    return obj;
 }
 
 // Criar um função para cada array de temperaturas
@@ -51,6 +98,6 @@ const getWeather = async (req, res) => {
 
 // 2ª - Função Temperatura de horas: 5 (próximas 5 horas)
 
-// 3ª - Função Temperatura de Dias: 5 (próximos 5 dias)
+// Previsão de Chuva para hoje
 
 module.exports = { getWeather };
---
+++ src/utils/tools.js
@@ -0,0 +1,4 @@
+
+const removeAllLetters = (string) => (string.replace(/[a-z A-Z]/g, ''));
+
+module.exports = { removeAllLetters }
