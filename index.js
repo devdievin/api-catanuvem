3719ac95a3e4ee8e7ab7eb4999bd03f6c3c01435
Dievano Dantas <dievano_dantas@hotmail.com>
2022-09-30 14:13:01 -0300

    cors enable

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require("path");
+const cors = require('cors');
 
 const routes = require('./src/routes/indexRoute');
 
@@ -8,9 +8,11 @@ const PORT = process.env.PORT || 4000;
 
 const app = express();
 
+app.use(cors());
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/', routes);
 
-app.listen(PORT, () => console.log(`Api running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Api running on port ${PORT}`));
